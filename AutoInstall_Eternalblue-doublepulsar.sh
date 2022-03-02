100682a69b1668fd779d82dcda5e2f5de7bd4c8e
Anonimo501 <67207446+Anonimo501@users.noreply.github.com>
2022-03-02 18:54:33 -0500

    Update AutoInstall_Eternalblue-doublepulsar.sh

--- AutoInstall_Eternalblue-doublepulsar.sh
+++ AutoInstall_Eternalblue-doublepulsar.sh
@@ -11,9 +11,11 @@ apt-get install winetricks -y
 dpkg --add-architecture i386 && apt-get update && apt-get install wine32 -y
 git clone https://github.com/ElevenPaths/Eternalblue-Doublepulsar-Metasploit
 cd Eternalblue-Doublepulsar-Metasploit/
+cp -r deps/ /usr/share/metasploit-framework/modules/exploits/windows/smb
 cp eternalblue_doublepulsar.rb /usr/share/metasploit-framework/modules/exploits/windows/smb
 mkdir -p /root/.wine/drive_c/
-cp -r deps/ /usr/share/metasploit-framework/modules/exploits/windows/smb
+wget http://download946.mediafire.com/8ko16qjdw2ug/yd7cose1ha7v8sm/pywin32-212.win32-py2.6.exe
+wine pywin32-212.win32-py2.6.exe
 
 echo ""
 echo -e "\e[31m*****************************************************************\e[0m"
